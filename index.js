7f1121e7214235e17b720f5d1cc03f94fce381c5
Epsys <90219425+R3GS@users.noreply.github.com>
2026-05-16 02:07:38 +0200

    index.js

--- index.js
+++ index.js
@@ -132,8 +132,6 @@ client.on('messageCreate', async (message) => {
             ? `Hmmm, l'animal spirituel de ${cible} est...`
             : "Hmmm, ton animal spirituel est...";
 
-        const isFem = Math.random() < 0.5;
-
     const animauxMasc = [
         "Un rat de RER", "Un pigeon", "Un chat errant", "Un renard", "Un dauphin", "Un corbeau", "Un hamster", "Un chien", "Un crapaud", "Un panda",
         "Un hérisson", "Un taureau", "Un papillon", "Un putain de moustique", "Un axolotl", "Un raton laveur", "Un perroquet", "Un singe",
