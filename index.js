870c728733616bee56d995fa374126e94c79aa92
Epsys <90219425+R3GS@users.noreply.github.com>
2026-05-17 19:06:01 +0200

    index.js

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ let birthdayData = { birthdays: {} };
 let dailyData = {};
 let weeklyData = {};
 
+fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${process.env.GEMINI_KEY}`)
+  .then(r => r.json())
+  .then(d => console.log(d.models?.map(m => m.name)));
+
 async function loadAll() {
     try {
         const res = await fetch(JSONBIN_URL + '/latest', {
