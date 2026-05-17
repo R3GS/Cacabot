70923c083e203eb0a94ffe51b32fc46698a115ff
Epsys <90219425+R3GS@users.noreply.github.com>
2026-05-17 19:03:29 +0200

    index.js

--- index.js
+++ index.js
@@ -1257,7 +1257,7 @@ async function sendBlague(interaction, cat, authorId) {
 
 async function callGemini(prompt) {
     try {
-        const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_KEY}`, {
+        const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_KEY}`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
